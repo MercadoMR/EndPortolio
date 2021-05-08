79464b36540fffa6b9d3a6aa6b0181d2d5331bb5
Martín Mercado <mmercadorg@gmail.com>
2021-05-07 19:57:33 -0500

    Basic Query/Mutation learning acquired. Now ready to develop!  B )

--- src/index.js
+++ src/index.js
@@ -4,17 +4,48 @@ const dotenv = require('dotenv');
 dotenv.config();
 const { ApolloServer, gql } = require('apollo-server-express');
 
+
+const comments=[ {id: '1', content: 'Muy buen post, me agrado', author: 'Marts'} ,
+                {id: '2', content: 'Malo, no me gusto', author: 'Okidoki'}
+]
+
 /** The Schema for our API */
 const typeDefs = gql`
-  type Query {
-    hello: String
+  type Query{
+    saludo: String!
+    comments: [Comment!]!
+    comment(id: ID!): Comment!
+  }
+  type Mutation{
+    newComment(content: String!): Comment!
+  }
+  type Comment{
+    id: ID!
+    content: String!
+    author: String
   }
 `;
+
 /** The resolver who responses the queries done against our API */
 const resolver = {
-    Query: {
-      hello: () => 'Hello world!',
+  Query: {
+      saludo: () => 'Hello world!',
+      comments: () => comments,
+      comment: (parent, args) =>{
+         return comments.find( comment => comment.id === args.id)
+      }
     },
+  Mutation: {
+    newComment: (parent, args) => {
+      let commentData ={
+        id: String(comments.length + 1),
+        content: args.content,
+        author: 'Mimirs'
+      };
+      comments.push(commentData);
+      return commentData;
+    }
+  }
 };
 
 const server = new ApolloServer({typeDefs,resolvers:resolver});
